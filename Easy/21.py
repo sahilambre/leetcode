88053afe1550bf6f3273ef147cb3b6f2d6ff27d4
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-06-24 22:51:22 -0400

    21

---
+++ Easy/21.py
@@ -0,0 +1,3 @@
+def mergeTwoLists(list1, list2):
+    merged = list1 + list2
+    return merged
---
+++ SandBox/347.top-k-frequent-elements.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=347 lang=python3
+#
+# [347] Top K Frequent Elements
+#
+
+# @lc code=start
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+# @lc code=end
+
