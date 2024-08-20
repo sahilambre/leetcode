5fdf0d565cd2437d6f10fa33f98bce2403b78161
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-08-19 22:38:56 -0400

    test.py

--- SandBox/test.py
+++
@@ -1,6 +0,0 @@
-def gcd(num1, num2):
-    while num2:           #! Continue loop until num2 becomes zero
-        num1, num2 = num2, num1 % num2 #* Swap num1 with num2, and num2 with the remainder of num1 divided by num2
-    return num1         #* When num2 becomes zero, num1 is the GCD
-
-print("Output:", gcd(24,32))
